d4a5fd1467fb4d4150d8ceb933adce1869f6f80c
rihab attig <rihab.attig@gmail.com>
2018-07-30 17:03:09 +0100

    first

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const bodyparser = require("body-parser");
+const port = 3000;
+
+app.use(bodyparser.json());
+
+app.get("/test", (req, res)=>{
+    res.send("just testing my function");
+})
+
+app.listen(`${port}`, err => console.log("I'm listening"));
+
